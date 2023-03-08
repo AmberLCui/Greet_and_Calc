1a44b874f17fa983ba5733300934438ec853e2c3
Amber Cui <97977446+AmberLCui@users.noreply.github.com>
2023-03-08 16:25:00 -0500

    Add files via upload

---
+++ calc/operations.py
@@ -0,0 +1,21 @@
+"""Basic math operations"""
+
+def add(a, b):
+    """Add a and b"""
+
+    return a + b
+
+def sub(a, b):
+    """Subtract a from b"""
+
+    return a - b
+
+def mult(a, b):
+    """Multiply a and b"""
+
+    return a * b
+
+def div(a, b):
+    """Divide a by b"""
+
+    return a / b
